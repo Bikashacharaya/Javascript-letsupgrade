63391fdad00c930bf7c8735cdb0a08398671f43d
Bikash Acharaya <63426263+Bikashacharaya@users.noreply.github.com>
2021-04-16 01:23:51 +0530

    Add files via upload

---
+++ Assignment-1/Q1.js
@@ -0,0 +1,8 @@
+const michael = "People say I'm the best boss.";
+
+//(michael.length) - 29
+//michael.slice(15) - the best boss.
+//michael.slice(11, 29) - I'm the best boss.
+//michael.slice(11, -1) - I'm the best boss
+//michael.slice(-5) - boss.
+//michael.slice(-10, -6) - best
---
+++ Assignment-1/Q2.js
@@ -0,0 +1,9 @@
+let dukeOfHastingsSays = "I burn for you.";
+let newArray = dukeOfHastingsSays.toLowerCase().split(" ");
+//1. newArrya = ["i", "burn", "for", "you."]
+
+let newStringWithSpace = newArray.join(" ");
+//2. newStringWithSpace = i burn for you.
+
+let newStringWithoutSpace = newArray.join("");
+//3.newStringWithoutSpace=iburnforyou.
