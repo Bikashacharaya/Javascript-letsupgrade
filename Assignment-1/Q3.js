37d7998c96858774d73630ed7f624f4817e4dc9c
Bikash Acharaya <63426263+Bikashacharaya@users.noreply.github.com>
2021-04-17 15:48:26 +0530

    Add files via upload

---
+++ Assignment-1/Q3.js
@@ -0,0 +1,17 @@
+let starHollows = ['Lorelai', 'Rory', 'Luke', 'Lane', 'Kirk', 'Taylor'];
+//let removed = starHollows.splice(1) = ["Rory", "Luke", "Lane", "Kirk", "Taylor"]
+
+let beverlyHillsGang = ['Brandon', 'Brenda', 'Dylan', 'Andrea', 'Steve', 'Kelly', 'Donna',
+'David'];
+
+//let removed = beverlyHillsGang.splice(1, 2); = ["Brenda", "Dylan"]
+
+let seinfeld = ['Jerry', 'George', 'Kramer', 'Newman', 'Elaine'];
+//let removed = seinfeld.splice(-2, 1); = ["Newman"]
+
+let brooklynNineNine = ['Jake', 'Amy', 'Charles','Rosa', 'Captain Holt', 'Gina', 'Terry'];
+//let removedAndInserted = brooklynNineNine.splice(5, 1, 'Hitchcock', 'Scully');= ["Gina"]
+
+
+let friends = ['Ross', 'Rachel', 'Joey', 'Chandler', 'Monica', 'Phoebe'];
+//let insertedOnly = friends.splice(1, 0, 'Gunther', 'Carol', 'Susan'); = []
